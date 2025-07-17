8b3b63d6285934f7fc7ca70940f1c697d4df7cd8
VAIBHAVSATPUTE <satputevaibhav582@gmail.com>
2025-07-17 22:47:00 +0530

    Update Modder.js
    Signed-off-by: VAIBHAVSATPUTE <satputevaibhav582@gmail.com>

--- Modder.js
+++ Modder.js
@@ -2,11 +2,5 @@
 const strings = {
   logoUrl: "https://i.ibb.co/4RjR0HwL/Sam-update.png",
   title: "InstaProMax V12.55 ✨",
-  message: "
-    [Base Update] 383.1.0.48.78 
-[Added] Option For Relocate Download buttons in feedposts
-[Added] Media Open By Phone App
-[Added] More UImods
-[Hidden] Enable Hidden Features 
-[Fixed] Many Random Crash.",
+  message: " [Base Update] 383.1.0.48.78 </b> [Added] Option For Relocate Download buttons in feedposts </b> [Added] Media Open By Phone App </b> [Added] More UImods </b> [Hidden] Enable Hidden Features </b> [Fixed] Many Random Crash.",
 };
